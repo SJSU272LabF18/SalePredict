446e268fd99499162f882839cfefafc3e9249dc3
Shreyam Kela <shreyamkela@gmail.com>
2018-11-20 02:53:33 -0800

    imports

---
+++ NLP_Operations/operations.py
@@ -0,0 +1,8 @@
+import nltk
+import string
+#nltk.download('stopwords')
+#nltk.download('punkt')
+from nltk.corpus import stopwords
+#from nltk.tokenize import word_tokenize
+from nltk.tokenize import RegexpTokenizer
+from collections import Counter
