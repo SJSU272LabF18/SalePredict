739c6b93ff3871557b3d95ca6f56007eb2fe00b8
Shreyam Kela <shreyamkela@gmail.com>
2018-11-21 20:42:59 -0800

    Training the Model

--- NLP_Operations/training_model.py
+++ NLP_Operations/training_model.py
@@ -7,7 +7,7 @@ import string
 from nltk.corpus import stopwords
 #from nltk.tokenize import word_tokenize
 from nltk.tokenize import RegexpTokenizer
-from nltk.stem.porter import *
+#from nltk.stem.porter import *
 from collections import Counter
 
 ''' NOTE - # is Commented Code and ## is just Comment'''
@@ -15,6 +15,7 @@ from collections import Counter
 stop_words = set(stopwords.words('english'))
 ## set used so that finding and removing stop words is faster
 #print (stop_words)
+## NOTE - In Stopwords text file we can add words such as app, application, apple, etc that are bound to have more frequency. In this way we might not have to use tf-idf from skikit learn but would this method be slower?
 
 sentence_1 = "My name is Clark Kent and I am currently a Masters student at the San Jose State University"
 sentence_2 = "Clark is doing his Masters study in the Software Engineering course. Started in the year 2018"
@@ -34,64 +35,32 @@ sentence_4 = sentence_4.lower()
 ## Tokenizer which also has the ability to get rid of punctualtions 
 tokenizer = RegexpTokenizer(r'\w+')
 
+## Initially we do not apply further preprocessing steps such as Stemming or Lemmatization and so on. First we train a basic model and then we can apply these advanced methods of text mining to increase accuracy. But would these modifications actually increase accuracy?
 
-'''
-## Stemming - Is Stemming required? Which one? If stemming is done then for eg. Unversity is stemmed to Univers which can be confused with Universal
-## Porter Stemmer
-stemmer = PorterStemmer()
-def stem_tokens(tokens, stemmer):
-    stemmed = []
-    for item in tokens:
-        if(item not in stop_words):
-            stemmed.append(stemmer.stem(item))
-    return stemmed    
-'''
-
-
-## POS tagging & Lemmatization - But with or without the stopwords and punctuation? - Does lemmatization require 'a', 'the', etc and such context to run properly?
-
-
-
-
-## Either use Stemming or use lemmatization
-
-
-words_tokens_1 = tokenizer.tokenize(sentence_1)
 ## Extracting and storing tokens from a token array while not storing stop words
-#filtered_sentence_1 = [w for w in words_tokens_1 if not w in stop_words]
-## Calling Stemming function
-#stemmed_1 = stem_tokens(words_tokens_1, stemmer)
-
+words_tokens_1 = tokenizer.tokenize(sentence_1)
+## Can increase performance by making our own regex tokenizer so that takenizing and and removing stop words takes place in the same iteration
+filtered_sentence_1 = [w for w in words_tokens_1 if not w in stop_words]
 words_tokens_2 = tokenizer.tokenize(sentence_2)
-
-
+filtered_sentence_2 = [w for w in words_tokens_2 if not w in stop_words]
 words_tokens_3 = tokenizer.tokenize(sentence_3)
-
-
+filtered_sentence_3 = [w for w in words_tokens_3 if not w in stop_words]
 words_tokens_4 = tokenizer.tokenize(sentence_4)
+filtered_sentence_4 = [w for w in words_tokens_4 if not w in stop_words]
 
+## NOTE - Can words_tokens_1 become a set so then when we run find when description is entered, it is O(1)
 
-count_1 = Counter(stemmed_1)
+count_1 = Counter(filtered_sentence_1)
 print(count_1)
 ## Gives frequency of each token in a 1D array of tokens (arranged in descending order)
 
-all_stemmed_words_tokens = [stemmed_1, stemmed_2, stemmed_3, stemmed_4]
-print (all_stemmed_words_tokens)
+all_words_tokens = [filtered_sentence_1, filtered_sentence_2, filtered_sentence_3, filtered_sentence_4]
+print (all_words_tokens)
 
+## tf-idf? To make document length same?
+## Cosine Similarity, euclidean distance, word/document to vector
 
-## NOTE - Can words_tokens_1 become a set so then when we run find  when description is entered, it is O(1)
-## NOTE - In Stopwords text file you can add words such as app, application, apple, etc that are bound to have more frequency. In this way you might not have to use tf-idf from skikit learn but would this method be slower?
-
-def jaccard_similarity(query, document):
-    intersection = set(query).intersection(set(document))
-    union = set(query).union(set(document))
-    return len(intersection)/len(union)
-#jaccard_similarity biased towards documents having larger length as union increases with increase in tokens
-#print (jaccard_similarity())
-
-# tf-idf?
-# Cosine and word to vector
-
-# Run all above functions on a new input
-# Weighted Average
-# Find Similarity
+## Run all above functions on a new input
+## Naive Bayes
+## Weighted Average
+## Find Similarity 
