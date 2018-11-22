585bf3a0a0fc2b61e3407518882dc0891a2fb098
Shreyam Kela <shreyamkela@gmail.com>
2018-11-21 20:46:10 -0800

    Jaccard's Similarity

---
+++ NLP_Operations/jaccard_similarity.py
@@ -0,0 +1,7 @@
+def jaccard_similarity(query, document):
+    intersection = set(query).intersection(set(document))
+    union = set(query).union(set(document))
+    return len(intersection)/len(union)
+## jaccard_similarity biased towards documents having larger length as union increases with increase in tokens
+## Can we make document lengths same by converting them to vectors and then apply this similarity?
+#print (jaccard_similarity(,))
