bbc925541c167767b3d10b2b69562540714a2e38
Shreyam Kela <shreyamkela@gmail.com>
2018-11-22 01:38:52 -0800

    Stemming

--- NLP_Operations/stemming.py
+++ NLP_Operations/stemming.py
@@ -1,7 +1,12 @@
 ## Either use Stemming or use lemmatization - Lemmatization would probably be better for our use case
 
 ## Stemming - Is Stemming required? Which one? If stemming is done then for eg. Unversity is stemmed to Univers which can be confused with Universal
-## Porter Stemmer
+## Porter Stemmer - Most widely used in NLP
+## NLTK Library used
+
+## Refer - https://www2.cs.duke.edu/courses/spring14/compsci290/assignments/lab02.html
+## Refer - https://www.datacamp.com/community/tutorials/text-analytics-beginners-nltk
+
 stemmer = PorterStemmer()
 def stem_tokens(tokens, stemmer):
     stemmed = []
