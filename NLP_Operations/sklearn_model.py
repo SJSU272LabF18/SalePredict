d0c78fa7cb7fb6195ea30cef1b11f6aa1f1d768d
Shreyam Kela <shreyamkela@gmail.com>
2018-11-23 00:22:20 -0800

    Approach - Finding Similarity

--- NLP_Operations/sklearn_model.py
+++ NLP_Operations/sklearn_model.py
@@ -26,6 +26,8 @@ vectorizer = TfidfVectorizer(stop_words = 'english')
 ## NOTE - does removing stop_words = 'english' above decrease accuracy of model?
 ## Edited the stop_words.py file. Would have to download the original stop_words.py if required in other projects
 
+## if df>>tf then is tfidf very very near 0 such that it wont show even with a double datatype? For this do we multiply tf by a constant to make it comparable to idf and make tfidf calculatable with avaiable datasets?
+
 ## tokenize and build vocabulary
 vectorizer.fit(sentence_array)
 
@@ -39,15 +41,25 @@ for i in range(len(sentence_array)):
     vector = vectorizer.transform([sentence_array[i]]) ## make sparse array
     ## summarize encoded vector
     all_documents_encoded.append(vector.toarray())
-    
-    
-    
+        
 ## print the range of a document vector
 print(vector.shape)
 ## print each document vector and its index   
 for j in range(len(all_documents_encoded)):
     print (all_documents_encoded[j], j)
     
+## NOTE - There are 2 approaches for creating or prediction model
+## 1st (could be more accurate) - Our model is a x-y plot in which each point represents a sparse array on x and the rating associated on y. Is it possible to assign a unique number to each sparse array? THis number would be on x and the associated rating would be on y. Then when form the whole graph and when a new sparse array (from new description) is entered then we match its y from the graph
+## 2nd - Our model finds k szimilar sparse arrays to our new sparse array. The ratings associated with theses k similar arrays are weighted averaged which gives us the prediced rating
+### In 2nd approach the innate idea is that we assume that the average rating of the apps matching the new description can probably be the potential rating. But in real life many other constraints act such as is the app free or is it paid, where is the app being release, etc and these constraints drive the app rating, which is not handled by this approach. It could be possible that the new description is very similar to a particular set of apps but when actually deployed, it gets very low ratings than actual due to some unforeseen reasons. This is somewhat handled by using weighted average. But then we should also keep in mind that ML is statistics only, and real world constraints and ambiguity cannot all be accounted for. Keep in mind that ML basically tells us how y should be for a new x in ideal conditions. But due to real world constraints, ideal case is not experienced generally. One thing we can do to take into the real world constraints is that when the averaging model is complete then we add improvements such as taking into account if the apps are paid or not, demographics of the app, technologies used and so on. 
+### The 1st approach is a regression approach which actually makes a plot of description on x and its corresponding rating on y. It could be possible that this plot accounts for all these different real world constraints, as it is essentialy making a plot of real world trend of how the apps do. But this is only a speculation, we're not sure exactly if the method would be able to predict a new description rating with decent accuracy or not. This method involves plotting the sparse arrays on the x axex different points. How do we do this? One method could be to take [0, 0, 0, ..] as origin and [1, 1, 1, ...] as the max x value. Now each sparse array is at a distance from the origin. But if we only take into account the euclidean distance then 2 different descriptions can have same distance from origin as when their arrays are plotted in x-y plane, they are at different angles from the origin but are at the same distance. So we can multiply the distance with the cosine angle to form unique real number value for each description that can be then plotted on the x axis in out regression plot, with rating on the y axis. But does this method might have some misconceptions that we cannot think of right now. 
+### Therefore we move forward with the second approach.
+
+## 2nd approach- Find Similarity: 
+## Do cosine similarity and euclidean distance have the same effect? Jaccard similarity? KNN?
+## Run all above functions on a new input
+## Find Weighted Average of Ratings
+
 '''
 ## Test Sentence 
 
@@ -57,14 +69,3 @@ vectorNew = vectorizer.transform(test_sentence)
 
 print(vectorNew)
 '''
-
-## Find Similarity: 
-## Do cosine similarity and euclidean distance have the same effect? Jaccard similarity? KNN?
-## Run all above functions on a new input
-## Naive Bayes?
-
-## Weighted Average
-
-## NOTE - There are 2 approaches for creating or prediction model
-## 1st (Probably more accurate) - Our model is a x-y plot in which each point represents a sparse array on x and the rating associated on y. Is it possible to assign a unique number to each sparse array? THis number would be on x and the associated rating would be on y. Then when form the whole graph and when a new sparse array (from new description) is entered then we match its y from the graph
-## 2nd - Our model finds k similar sparse arrays to our new sparse array. The ratings associated with theses k similar arrays are weighted averaged which gives us the prediced rating
