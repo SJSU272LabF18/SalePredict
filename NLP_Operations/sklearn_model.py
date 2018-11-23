aaebc3580ae9cdda79a2f7672957b13ce78f0d57
Shreyam Kela <shreyamkela@gmail.com>
2018-11-23 14:51:58 -0800

    Update sklearn_model.py

--- NLP_Operations/sklearn_model.py
+++ NLP_Operations/sklearn_model.py
@@ -1,7 +1,9 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction import text
+
 #import numpy as np
-''' NOTE - # is Commented Code and ## is just Comment'''
+''' NOTE - # is Commented Code and ## is just Comment and ### is Sub-Comment for the nearest ## above'''
 
 ## Refer - https://machinelearningmastery.com/prepare-text-data-machine-learning-scikit-learn/
 ## countVectorizer - does it lowercase and remove punc and remove stopwords for whole documents with paragraphs? Stopwords?
@@ -15,7 +17,7 @@ sentence_2 = "Clark is doing his Masters study in the Software Engineering cours
 sentence_3 = "My name is Martha Kent. I am an employee at Google, Mountain View"
 sentence_4 = "Martha is currently a Programmer Analyst at Google. Started in the year 2015"
 
-test_sentence = "The quick brown fox clark jumps over the lazy dog. Carpe diem!"
+test_sentence = "The quick brown fox Clark jumps over the lazy dog Martha. Carpe diem!"
 
 sentence_array = [sentence_1,sentence_2,sentence_3,sentence_4]
 
@@ -29,7 +31,7 @@ vectorizer = TfidfVectorizer(stop_words = 'english')
 ## NOTE - does removing stop_words = 'english' above decrease accuracy of model?
 ## Edited the stop_words.py file. Would have to download the original stop_words.py if required in other projects
 
-## if df>>tf then is tfidf very very near 0 such that it wont show even with a double datatype? For this do we multiply tf by a constant to make it comparable to idf and make tfidf calculatable with avaiable datasets?
+## if df>>tf then is tfidf very very near 0 such that it wont show even with a double datatype? For this do we multiply tf by a constant to make it comparable to idf and make tfidf calculatable for avaiable datasets?
 
 ## tokenize and build vocabulary
 vectorizer.fit(sentence_array)
@@ -51,20 +53,32 @@ print(vector.shape)
 for j in range(len(all_documents_encoded)):
     print (all_documents_encoded[j], j)
     
-## Handling unseen words in the test sentence
+## Handling unseen words, stop words, punctuations, and next lines in the test sentence
 tokenizer = vectorizer.build_tokenizer()
 test_sentence_tokens = tokenizer(test_sentence)
-print (test_sentence_tokens)
+print (test_sentence_tokens)    
 
-'''
+## Store all words from sklearn stop words file
+stop_words = text.ENGLISH_STOP_WORDS
+
+test_sentence_tokens_filtered = []
 for i in range(len(test_sentence_tokens)):
-    if(test_sentence_tokens[i].lower() not in stop_words && vectorizer.vocabulary_.get(test_sentence_tokens[i].lower())!= None)
-    ## to lower and stop word remove
-    
-'''
-    
-print (test_sentence_tokens[4].lower())
-print (vectorizer.vocabulary_.get(test_sentence_tokens[4].lower()))
+    ## if the current token lowercased is not in stop words and is present in vocabulary, then append the token to a token array
+    if not test_sentence_tokens[i].lower() in stop_words and vectorizer.vocabulary_.get(test_sentence_tokens[i].lower()) != None :
+        test_sentence_tokens_filtered.append(test_sentence_tokens[i].lower())
+
+print (test_sentence_tokens_filtered)
+
+## vectorizer.transform takes in string to form a sparse array of it
+## Therefore, we convert tbe te sentence token array to string
+
+test_sentence_modified = " ".join(test_sentence_tokens_filtered)
+## "".join(['a','b','c']) means Join all elements of the array, separated by the string "". In the same way, " hi ".join(["jim", "bob", "joe"]) will create "jim hi bob hi joe"
+
+test_sentence_vector = vectorizer.transform([test_sentence_modified]) ## make sparse array
+test_sentence_encoded = (test_sentence_vector.toarray())
+
+print (test_sentence_encoded)
 
 ## NOTE - There are 2 approaches for creating or prediction model
 ## 1st (could be more accurate) - Our model is a x-y plot in which each point represents a sparse array on x and the rating associated on y. Is it possible to assign a unique number to each sparse array? THis number would be on x and the associated rating would be on y. Then when form the whole graph and when a new sparse array (from new description) is entered then we match its y from the graph
@@ -101,4 +115,3 @@ y = all_documents_encoded[4]
 #y = y.reshape(1,-1)
 
 cosine_similarity(x,y)
-
