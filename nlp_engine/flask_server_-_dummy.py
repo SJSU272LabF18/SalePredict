2e5bbbe21fa4c7fe6bd8bc1fc844451e23d42c27
Shreyam Kela <shreyamkela@gmail.com>
2018-11-28 00:49:35 -0800

    dummy server

--- nlp_engine/flask_server_-_dummy.py
+++ nlp_engine/flask_server_-_dummy.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 ## Refer - https://www.analyticsvidhya.com/blog/2017/09/machine-learning-models-as-apis-using-flask/
 ## Refer - https://projects.raspberrypi.org/en/projects/python-web-server-with-flask/2
+## Refer - https://www.youtube.com/results?search_query=machine+learning+model+deployment+using+flask
 
 app = Flask(__name__) 
 ## __name__ refers to main
@@ -18,6 +19,19 @@ def index(): ## main webpage
 def test(name):
     print ("HI") ## prints on console
     return ("Hello {}!".format(name)) ## prints on browser
+
+
+@app.route('/json_description/<string>', methods=['POST']) ## Webpage which accepts POST json data and also prints the string in url
+## http://127.0.0.1:5000/json_description/shrke 
+## Refer - http://toolsqa.com/postman/post-request-in-postman/
+## Refer - https://stackoverflow.com/questions/20001229/how-to-get-posted-json-in-flask
+def json_description(string):
+    print ("Hey") ## prints on console
+    content = request.json ## request json data from body
+    print (content['description']) ## print on console, json data which has"description" as key
+    #return jsonify(content) ## prints on browser, json syntax that was POSTed
+    return string ## prints on browser
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
