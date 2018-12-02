4df0140fe61be6526d95d381e8a1e436c4cb24df
Shreyam Kela <shreyamkela@gmail.com>
2018-12-01 19:48:05 -0800

    Adding number of users by rating

--- nlp_engine/similarity_with_manual_topX_sort.py
+++ nlp_engine/similarity_with_manual_topX_sort.py
@@ -35,6 +35,8 @@ rating_array = pickle.load(open("rating_array.pickle", "rb"))
 ## Load all track name array
 track_name_array = pickle.load(open("track_name_array.pickle", "rb"))
 #print (track_name_array[0])
+## Load rating count for each description
+rating_count_array = pickle.load(open("rating_count_tot.pickle", "rb"))
 ## Load All Documents Encoded Sparse Array npz file
 sparse_matrix = scipy.sparse.load_npz('sparse_matrix_actual.npz')
 all_documents_encoded = sparse_matrix.todense()
@@ -111,29 +113,47 @@ def find_similarity(test_description_modified):
     print (all_documents_similarity_sorted_topXpercent[0][0][0][0])
     
     ## If highest similarity is < say 0.2 then tell the user to add more description for better results? - So as to tackle single word or       line descriptions. To tackle persistant users, add a button if they want analytics with only that much of description?
-    if(all_documents_similarity_sorted_topXpercent[0][0][0][0] < 0.2):
-        print ("For better analytics, enter more description specific to your app")
+    if(all_documents_similarity_sorted_topXpercent[0][0][0][0] < 0.35):
+        print ("For better analytics, enter more description specific to your app idea")
 
     ## Print index of the description found to be similar
     #print (all_documents_similarity_sorted_topXpercent[0][1])
 
-    ## Link Datasets and Find Weighted Average of Ratings
+    ## Link Datasets and Find Weighted Average of Ratings and other details
     total_weight = 0
     total_weighted_rating = 0
+    users_by_rating_dict = { "0.5":0, "1.0":0, "1.5":0, "2.0":0, "2.5":0, "3.0":0, "3.5":0, "4.0":0, "4.5":0, "5.0":0 }
+    total_users_that_rated = 0
     for i in range(len(all_documents_similarity_sorted_topXpercent)):
         #document_rating = data_full.iloc[(all_documents_similarity_sorted_topXpercent[i][1])]['user_rating']
         document_rating = rating_array[(all_documents_similarity_sorted_topXpercent[i][1])]
+        document_rating_count = rating_count_array[(all_documents_similarity_sorted_topXpercent[i][1])]
+        if document_rating_count == 0:
+            continue
         if document_rating == 0:
             continue
         document_name = track_name_array[(all_documents_similarity_sorted_topXpercent[i][1])]
         document_id = all_documents_similarity_sorted_topXpercent[i][1]
-        print (document_id, document_name, document_rating)
-        document_weight = all_documents_similarity_sorted_topXpercent[i][0][0][0]
+        print ("id:",document_id, "name:", document_name, "rating:", document_rating, "rating_count:", document_rating_count, "similarity:", all_documents_similarity_sorted_topXpercent[i][0][0][0])
+        
+        ## Find the final Average rating - Weighted average of ratings of topXpercent similar documents 
+        ## Considering Document weight = Similarity Score multiplied by document_rating_count
+        document_weight = all_documents_similarity_sorted_topXpercent[i][0][0][0]*document_rating_count
         document_weighted_rating = document_weight*document_rating
         total_weighted_rating = total_weighted_rating+document_weighted_rating
         total_weight = total_weight+document_weight
-
+        
+        ## For the graph of "number of users" by "Rating Given" - 
+        this_rating = str(document_rating)
+        users_at_this_rating = users_by_rating_dict[this_rating]
+        users_by_rating_dict[this_rating] = int(users_at_this_rating + document_weight)
+        print ("users:", document_weight, "rating:", document_rating, "previous users at this rating:", users_at_this_rating, "new users at this rating", users_by_rating_dict[this_rating])
+        total_users_that_rated = total_users_that_rated + document_weight
+        
+        
     final_rating = total_weighted_rating/total_weight
+    print("Users by rating:", users_by_rating_dict)
+    print("Total users that are likely to rate: ", int(total_users_that_rated))
     return final_rating
    
         
@@ -152,11 +172,7 @@ def sortTopXpercent(all_documents_similarity, topXpercent):
                 temp = all_documents_similarity[i]
                 all_documents_similarity[i] = all_documents_similarity[i-1]
                 all_documents_similarity[i-1] = temp
-
-            
-
-
-
+                
         x = x+1
 
     all_documents_similarity_sorted_topXpercent = all_documents_similarity[:topXpercent]
@@ -173,7 +189,8 @@ print ("FILES LOADED", track_name_array[0])
 
 start_time = time.time()
 
-test_description = "Bank from almost anywhere with the Chase Mobile® app. Manage your accounts, pay bills, pay people, deposit checks, transfer money, all safely and securely with your device. Whether you’re on the go or curled up on the couch, day or night, with the Chase Mobile app, you’re in control of your accounts. CHECK YOUR ACCOUNT WITH JUST A FEW TAPS • Simply swipe to see your balances without signing in using Account Preview. • Sign in securely with fingerprint sign in and don’t worry about fat-fingering your password. • Monitor your checking, savings or credit card activities. • Keep track of your credit and debit card rewards that you’re saving for your next vacation. MANAGE YOUR ACCOUNTS WHEN YOU’RE ON THE GO • Set up a travel notification for your debit and credit cards to let us know if you’ll be away. • Opt in to Account Alerts and set your notification preferences so you can keep tabs on your account activity. MOVE MONEY WITHOUT STEPPING INSIDE A BRANCH • See how much you owe and when, plus schedule a payment for your Chase credit card, mortgage or other loan or line of credit. • Pay other bills such as utilities, service providers, and credit cards. • Use Chase QuickPay® with Zelle® to pay back your friends for lunch, your babysitter for the week or nudge your brother to pay you back for mom’s birthday gift. • Snap a picture and deposit checks with Chase QuickDeposit®. • Transfer money between your Chase accounts. • Reload your Chase Liquid® prepaid card. • Send wire transfers. CONNECT WITH CHASE • Find the nearest Chase branches and ATMs and get directions. • Speak with a service representative 24/7."
+test_description = "SAVE 20%, now only $3.99 for a limited time! One of the most popular video games in arcade history! 2015 World Video Game Hall of Fame Inductee Who can forget the countless hours and quarters spent outrunning pesky ghosts and chompinâ€™ on dots? Now you can have the same arcade excitement on your mobile devices! Guide PAC-MAN through the mazes with easy swipe controls, a MFi controller, or kick it old school with the onscreen joystick! Eat all of the dots to advance to the next stage. Go for high scores and higher levels! Gain an extra life at 10.000 points! Gobble Power Pellets to weaken ghosts temporarily and eat them up before they change back. Avoid Blinky, the leader of the ghosts, and his fellow ghosts Pinky, Inky, and Clyde, or you will lose a life. Itâ€™s game over when you lose all your lives. 9 NEW MAZES Included!!! The game includes 9 new mazes in addition to the pixel for pixel recreation of the classic original maze. Challenge your skill to beat them all! We are constantly updating the game with new maze packs that you can buy to complete your PAC-MAN collection. HINTS and TIPS!!! Insider pro-tips and hints are being made available for the first time in-game! Use these to help you become a PAC-MAN champion! FEATURES: â€¢ New tournaments â€¢ New Visual Hints and Pro-tips â€¢ New mazes for all new challenges â€¢ Play an arcade perfect port of classic PAC-MAN â€¢ Two different control modes â€¢ Three game difficulties (including the original 1980 arcade game) â€¢ Retina display support â€¢ MFi controller support"
+
 if detect(test_description) != 'en':
     print ("Error: Please enter plain english detailed description")
     exit()
@@ -197,3 +214,23 @@ final_rating = find_similarity(test_description_modified)
 
 print (final_rating)
 print("--- %s seconds ---" % (time.time() - start_time))
+
+## Page 1:
+## Show success or failure
+## show average rating rounded off
+## Show your pecentile/ranking w.r.t all other apps in the app store. And w.r.t genre as well?
+## Show genre of app?
+## Show potential number of total installs
+## Show potential number of total users that will rate
+
+
+## Page 2:
+## show graph of number of users by rating
+
+## Page 3:
+## 
+
+## All pages:
+## show top 3 apps similar and their description excript (most important text summarized?) and their similarity percentage, and their number of installs
+## For more detailed analysis - sign up/login and premium
+## Top 3 Free or paid? whether you should put a prize on your app?
