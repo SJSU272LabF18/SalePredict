68a6a481e9c2f4bde9af2b0985e40a6880f5b928
Shreyam Kela <shreyamkela@gmail.com>
2018-12-02 15:16:20 -0800

    Installs by age group

--- nlp_engine/similarity_with_manual_topX_sort.py
+++ nlp_engine/similarity_with_manual_topX_sort.py
@@ -77,6 +77,39 @@ def get_wordnet_pos(treebank_pos):
         #return ''        
 
         
+
+        
+def sortTopXpercent(all_documents_similarity, topXpercent):
+
+    x = 1
+    while(x<=topXpercent):
+
+        for i in range(len(all_documents_similarity)-1, -1, -1):
+            
+            if(i == 0):
+                continue
+            if(all_documents_similarity[i]>all_documents_similarity[i-1]):
+                temp = all_documents_similarity[i]
+                all_documents_similarity[i] = all_documents_similarity[i-1]
+                all_documents_similarity[i-1] = temp
+                
+        x = x+1
+
+    all_documents_similarity_sorted_topXpercent = all_documents_similarity[:topXpercent]
+
+    return all_documents_similarity_sorted_topXpercent
+           
+        
+## For the graph of "number of users" by "Age Group" - 
+def age_group_percent(users_by_ageGroup_dict, users_by_ageGroup_dict_percent, document_weight):
+    for x in users_by_ageGroup_dict:
+        temp3 = users_by_ageGroup_dict[x]
+        users_by_ageGroup_dict[x] = int(temp3 + (document_weight*users_by_ageGroup_dict_percent[x]))
+    return users_by_ageGroup_dict        
+
+        
+        
+        
 def find_similarity(test_description_modified):
     ## make sparse array of filtered test description
     test_document_vector = vectorizer.transform([test_description_modified])
@@ -127,7 +160,7 @@ def find_similarity(test_description_modified):
     users_by_rating_dict = { "0.5":0, "1.0":0, "1.5":0, "2.0":0, "2.5":0, "3.0":0, "3.5":0, "4.0":0, "4.5":0, "5.0":0 }
     users_by_ageGroup_dict = { "Children_5-13":0, "Teenager_13-18":0, "Adult_18-50":0, "Elderly_50+":0}
     total_users_that_rated = 0
-    ## Arbitrary installs factor value - Apple dataset does not contain information about total installs so have to calculate an average value assuming that every 1 person among 'total_users_that_rated/installs_factor' number of persons rates the app
+    ## Arbitrary installs factor value, , through intuition - Apple dataset does not contain information about total installs so have to calculate an average value assuming that every 1 person among 'total_users_that_rated/installs_factor' number of persons rates the app
     installs_factor = 250
     for i in range(len(all_documents_similarity_sorted_topXpercent)):
         #document_rating = data_full.iloc[(all_documents_similarity_sorted_topXpercent[i][1])]['user_rating']
@@ -141,6 +174,7 @@ def find_similarity(test_description_modified):
         document_id = all_documents_similarity_sorted_topXpercent[i][1]
         print ("id:",document_id, "name:", document_name, "rating:", document_rating, "rating_count:", document_rating_count, "similarity:", all_documents_similarity_sorted_topXpercent[i][0][0][0])
         
+        
         ## Find the final Average rating - Weighted average of ratings of topXpercent similar documents 
         ## Considering Document weight = Similarity Score multiplied by document_rating_count
         document_weight = all_documents_similarity_sorted_topXpercent[i][0][0][0]*document_rating_count
@@ -148,24 +182,37 @@ def find_similarity(test_description_modified):
         total_weighted_rating = total_weighted_rating+document_weighted_rating
         total_weight = total_weight+document_weight
         
+        
         ## For the graph of "number of users" by "Rating Given" - 
         this_rating = str(document_rating)
         users_at_this_rating = users_by_rating_dict[this_rating]
         users_by_rating_dict[this_rating] = int(users_at_this_rating + document_weight)
-        print ("users:", document_weight, "rating:", document_rating, "previous users at this rating:", users_at_this_rating, "new users at this rating", users_by_rating_dict[this_rating])
+        
+        
         total_users_that_rated = total_users_that_rated + document_weight
         
         
-        ## For the graph of "number of users" by "Age Group" - 
+        ## For the graph of "number of users" by "Age Group" -  
+        ## According to the 4 diff content ratings, we make 4 diff dict of percentages of usage for the 4 age groups
+        ## Arbitrary average percentage values, through intuition- Apple dataset does not contain information about total user rating per age group or total installs per age group, for each app. Therefore we use arbitrary average values
         age_group = age_group_array[(all_documents_similarity_sorted_topXpercent[i][1])]
-        if(age_group == "4+" or age_group == None or or age_group == "0"):
-            
+        if(age_group == "4+" or age_group == None or age_group == "0"):
+            users_by_ageGroup_dict_percent = { "Children_5-13":0.05, "Teenager_13-18":0.25, "Adult_18-50":0.55, "Elderly_50+":0.15}
+            users_by_ageGroup_dict = age_group_percent(users_by_ageGroup_dict, users_by_ageGroup_dict_percent, document_weight)     
         elif(age_group == "9+"):
-            
-        elif(age_group == "12+"):  
-            
+            users_by_ageGroup_dict_percent = { "Children_5-13":0.03, "Teenager_13-18":0.27, "Adult_18-50":0.55, "Elderly_50+":0.15}
+            users_by_ageGroup_dict = age_group_percent(users_by_ageGroup_dict, users_by_ageGroup_dict_percent, document_weight)        
+        elif(age_group == "12+"): 
+            users_by_ageGroup_dict_percent = { "Children_5-13":0.001, "Teenager_13-18":0.25, "Adult_18-50":0.599, "Elderly_50+":0.15}
+            users_by_ageGroup_dict = age_group_percent(users_by_ageGroup_dict, users_by_ageGroup_dict_percent, document_weight)        
         elif(age_group == "17+"): 
+            users_by_ageGroup_dict_percent = { "Children_5-13":0.0, "Teenager_13-18":0.05, "Adult_18-50":0.8, "Elderly_50+":0.15}
+            users_by_ageGroup_dict = age_group_percent(users_by_ageGroup_dict, users_by_ageGroup_dict_percent, document_weight)
         
+        #print ("users:", document_weight, "rating:", document_rating, "previous users at this rating:", users_at_this_rating, "new users at this rating", users_by_rating_dict[this_rating])
+        print ("users:", document_weight, "rating:", document_rating, "Content rating:", age_group)
+        
+        print ("Current users_by_ageGroup_dict:", users_by_ageGroup_dict)
         
     ## Final Average rating    
     final_rating = total_weighted_rating/total_weight
@@ -185,44 +232,29 @@ def find_similarity(test_description_modified):
     factor = total_users_that_rated/installs_factor
     print("Total installs: ", int(total_users_that_rated*factor))
     
+    ## Total predicted "users that rated" ordered by age
+    print ("Users that rated, ordered by age:", users_by_ageGroup_dict)
     
+    ## Total predicted "installs" ordered by age 
+    for x in users_by_ageGroup_dict:
+        temp4 = users_by_ageGroup_dict[x]
+        users_by_ageGroup_dict[x] = temp4*factor
+    print ("Installs, ordered by age:", users_by_ageGroup_dict)
     
     return final_rating
    
         
 
         
-def sortTopXpercent(all_documents_similarity, topXpercent):
-
-    x = 1
-    while(x<=topXpercent):
-
-        for i in range(len(all_documents_similarity)-1, -1, -1):
-            
-            if(i == 0):
-                continue
-            if(all_documents_similarity[i]>all_documents_similarity[i-1]):
-                temp = all_documents_similarity[i]
-                all_documents_similarity[i] = all_documents_similarity[i-1]
-                all_documents_similarity[i-1] = temp
-                
-        x = x+1
-
-    all_documents_similarity_sorted_topXpercent = all_documents_similarity[:topXpercent]
-
-    return all_documents_similarity_sorted_topXpercent
-           
         
         
-        
-        
-        
-print ("FILES LOADED", track_name_array[0])
+print ("FILES LOADED")
 
 
 start_time = time.time()
 
-test_description = "WhatsApp Messenger is a FREE messaging app available for Android and other smartphones. WhatsApp uses your phone's Internet connection (4G/3G/2G/EDGE or Wi-Fi, as available) to let you message and call friends and family. Switch from SMS to WhatsApp to send and receive messages, calls, photos, videos, documents, and Voice Messages. WHY USE WHATSAPP: • NO FEES: WhatsApp uses your phone's Internet connection (4G/3G/2G/EDGE or Wi-Fi, as available) to let you message and call friends and family, so you don't have to pay for every message or call.* There are no subscription fees to use WhatsApp. • MULTIMEDIA: Send and receive photos, videos, documents, and Voice Messages. • FREE CALLS: Call your friends and family for free with WhatsApp Calling, even if they're in another country.* WhatsApp calls use your phone's Internet connection rather than your cellular plan's voice minutes. (Note: Data charges may apply. Contact your provider for details. Also, you can't access 911 and other emergency service numbers through WhatsApp). • GROUP CHAT: Enjoy group chats with your contacts so you can easily stay in touch with your friends or family. • WHATSAPP WEB: You can also send and receive WhatsApp messages right from your computer's browser. • NO INTERNATIONAL CHARGES: There's no extra charge to send WhatsApp messages internationally. Chat with your friends around the world and avoid international SMS charges.* • SAY NO TO USERNAMES AND PINS: Why bother having to remember yet another username or PIN? WhatsApp works with your phone number, just like SMS, and integrates seamlessly with your phone's existing address book. • ALWAYS LOGGED IN: With WhatsApp, you're always logged in so you don't miss messages. No more confusion about whether you're logged in or logged out. • QUICKLY CONNECT WITH YOUR CONTACTS: Your address book is used to quickly and easily connect you with your contacts who have WhatsApp so there's no need to add hard-to-remember usernames. • OFFLINE MESSAGES: Even if you miss your notifications or turn off your phone, WhatsApp will save your recent messages until the next time you use the app. • AND MUCH MORE: Share your location, exchange contacts, set custom wallpapers and notification sounds, email chat history, broadcast messages to multiple contacts at once, and more! *Data charges may apply. Contact your provider for details."
+test_description = "With 30 billion matches to date, Tinder® is the world’s most popular app for meeting new people. Think of us as your most dependable wingmate—wherever you go, we’ll be there. If you’re here to meet new people, expand your social network, meet locals when you’re traveling, or just live in the now, you’ve come to the right place. We’re called “the world’s hottest app” for a reason: we spark more than 26 million matches per day. How many dating apps do that? Match. Chat. Date. Tinder is easy and fun—use the Swipe Right™ feature to Like someone, use the Swipe Left™ feature to pass. If someone likes you back, It’s a Match! We invented the double opt-in so that two people will only match when there’s a mutual interest. No stress. No rejection. Just tap through the profiles you’re interested in, chat online with your matches, then step away from your phone, meet up in the real world and spark something new. Now, let’s get started. And remember, when in doubt, Swipe Right. Trust us, the more options you have, the better-looking life becomes. Welcome to Tinder—the largest, hottest community of singles in the world. Don’t be shy, come on over. MORE FEATURES? THAT’S A PLUS Upgrade to Tinder Plus® for premium features, including: Unlimited Likes so you can use the Swipe Right feature to your heart’s content, Passport to chat with singles anywhere around the world, Rewind to give someone a second chance, one free Boost per month to be the top profile in your area for 30 minutes, and additional Super Likes to stand out from the crowd. GET THE GOLD TREATMENT Upgrade to Tinder Gold™ for a first-class experience: Passport, Rewind, Unlimited Likes, five Super Likes per day, one Boost per month, and more profile controls. But wait, it gets better. Save time and aimless searching with our Likes You feature, which lets you see who likes you. Think of it as your personal Tinder concierge—available 24/7—bringing all of your pending matches to you. Now you can sit back, enjoy a fine cocktail, and browse through profiles at your leisure. Goodbye search fatigue. Hello #GoldLife ----------------------------------- If you choose to purchase Tinder Plus or Tinder Gold, payment will be charged to your Google Play account, and your account will be charged for renewal within 24-hours prior to the end of the current period. Auto-renewal may be turned off at any time by going to your settings in the Play Store after purchase. Current Tinder Plus subscription price starts at $9.99 USD/month, and one-month, 6-month and 12-month packages are available. Current Tinder Gold subscription price starts at $14.99 USD/month, and one-month, 6-month and 12-month packages are available. Prices are in U.S. dollars, may vary in countries other than the U.S. and are subject to change without notice. No cancellation of the current subscription is allowed during the active subscription period. If you don’t choose to purchase Tinder Plus or Tinder Gold, you can simply continue using Tinder for free"
+
 
 if detect(test_description) != 'en':
     print ("Error: Please enter plain english detailed description")
@@ -233,7 +265,7 @@ else:
     print ("HI")
 
 test_description_modified = lemmatized_Description
-print (test_description_modified)
+#print (test_description_modified)
 
 
 ## SORT ONLY THOSE MUCH SIMILARITIES THAT ARE NEEDED - all_documents_similarity_sorted - dont sort all 7000 entires
@@ -252,7 +284,6 @@ else:
     print ("FAILURE")
 print("--- %s seconds ---" % (time.time() - start_time))
 
-
 ## Frontend display:
 ### Page 1:
 #### Show success or failure
@@ -264,7 +295,7 @@ print("--- %s seconds ---" % (time.time() - start_time))
 ### Page 2:
 #### show graph of number of users by rating
 ### Page 3:
-#### Graph of users by age group
+#### Pie Chart of users by age group
 ### All pages:
 #### show top 3 apps similar and their description excript (most important text summarized?) and their similarity percentage, and their number of installs
 #### For more detailed analysis - sign up/login and premium
