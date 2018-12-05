d051cf04a0014ed38e274d89f7bdc730ce16e6a8
saybee <sayaleebhusari@gmail.comgit config --global user.email sayaleebhusari@gmail.com>
2018-12-05 13:53:05 -0800

    UI changes

--- ReactFrontend/src/components/Home.js
+++ ReactFrontend/src/components/Home.js
@@ -1,12 +1,93 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { Navbar, Nav, NavDropdown, MenuItem, FormGroup, FormControl, InputGroup, DropdownButton } from 'react-bootstrap';
+import axios from 'axios';
+import '../css/MainPage.css';
+
+
 
 class Home extends Component {
-    state = {}
+    constructor() {
+        super();
+        this.state = {
+        };
+        this.submitDescription = this.submitDescription.bind(this);
+        this.onChange = this.onChange.bind(this);
+        this.getRatings = this.getRatings.bind(this);
+
+    }
+    // componentDidMount() {
+
+    // }
+    getRatings() {
+        this.props.history.push("/ratingschart");
+    }
+    onChange(e) {
+
+        this.setState({
+            [e.target.name]: e.target.value
+
+        })
+    }
+    submitDescription() {
+        var data = {
+            description: this.state.description
+        }
+
+        // var newdata = bodyFormData.set('description', this.state.description);
+        console.log('data ' + JSON.stringify(data))
+        // axios.post(`http://localhost:5000/json_description`, querystring.stringify({ description: this.state.description }))
+        axios.post(`http://localhost:5000/json_description`, data)
+
+            .then((response) => {
+                console.log(" response.data" + response)
+
+                console.log(" response.data" + JSON.stringify(response.data))
+                localStorage.setItem("data", JSON.stringify(response.data))
+
+            })
+    }
     render() {
         return (<div>
+            <Navbar inverse collapseOnSelect className='navbar-fixed-top navbarlinkedin-navbar'>
+                <Navbar.Header>
+                    <Navbar.Brand>
+                        <Link to="/home"><img alt="linkedin-logo" src="MainPage.jpeg" height="30" width="35"></img></Link>
+                    </Navbar.Brand>
+                    <Navbar.Toggle />
+                </Navbar.Header>
+                <Navbar.Collapse>
+                    <Nav pullRight>
+                        {/* <NavItem eventKey={1} href="#">
+                        Link</NavItem>
+                    <NavItem eventKey={2} href="#">
+                        Link</NavItem> */}
+                        <NavDropdown eventKey={3} title="Account" id="basic-nav-dropdown" >
+                            <MenuItem eventKey={3.1}><Link to="/login"><span className='glyphicon glyphicon-log-in'></span>    Login</Link></MenuItem>
+                            <MenuItem eventKey={3.2}><Link to='#'><span className='glyphicon glyphicon-user'></span>    Profile </Link></MenuItem>
+                            <MenuItem eventKey={3.3}><Link to="/mainpage"><span className='glyphicon glyphicon-signal'></span>   Check your App's Success</Link></MenuItem>
+                            <MenuItem divider />
+                            <MenuItem eventKey={3.4}><Link to="/home"><span className='glyphicon glyphicon-home'></span>   Home</Link></MenuItem>
+                        </NavDropdown>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+            <div className='col-md-4 left-color'>
+            </div>
+            <div className='col-md-4'>
 
-            <Link to='/mainpage'><img src='MainPage.jpeg'></img></Link>
+                <div className='container new-container'>
+                    <h1 className='main-page-h1'>Will It Sell!</h1>
+                    <textarea placeholder="Enter Description" className='mainpage-textarea' name='description' onChange={this.onChange}></textarea>
+                    <div>
+                        <button className='btn btn-primary mainpage-btn' onClick={this.submitDescription}>Submit</button>
+
+                    </div>
+                </div>
+            </div>
+            <div className='col-md-4'>
+            </div>
+
 
         </div>);
     }
--- ReactFrontend/src/components/MainPage.js
+++ ReactFrontend/src/components/MainPage.js
@@ -1,27 +1,27 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import '../css/MainPage.css';
-import WisNavbar from './WisNavbar';
-//var bodyFormData = new FormData();
-var querystring = require('querystring');
+import { Link } from 'react-router-dom';
 
+import { Navbar, Nav, NavDropdown, MenuItem, FormGroup, FormControl, InputGroup, DropdownButton } from 'react-bootstrap';
+
+import AgeGroupChart from './AgeGroupChart';
+import RatingsChart from './RatingsChart';
 
 class MainPage extends Component {
     constructor() {
         super();
         this.state = {
+            description: "",
+            loading: false,
+            next: false
+
         };
         this.submitDescription = this.submitDescription.bind(this);
         this.onChange = this.onChange.bind(this);
-        this.getRatings = this.getRatings.bind(this);
 
     }
-    // componentDidMount() {
 
-    // }
-    getRatings() {
-        this.props.history.push("/ratingschart");
-    }
     onChange(e) {
 
         this.setState({
@@ -29,7 +29,8 @@ class MainPage extends Component {
 
         })
     }
-    submitDescription() {
+    async submitDescription() {
+        this.setState({ loading: true });
         var data = {
             description: this.state.description
         }
@@ -37,34 +38,119 @@ class MainPage extends Component {
         // var newdata = bodyFormData.set('description', this.state.description);
         console.log('data ' + JSON.stringify(data))
         // axios.post(`http://localhost:5000/json_description`, querystring.stringify({ description: this.state.description }))
-        axios.post(`http://localhost:5000/json_description`, data)
+        const response = await axios.post(`http://localhost:5000/json_description`, data)
 
-            .then((response) => {
-                console.log(" response.data" + response)
 
-                console.log(" response.data" + JSON.stringify(response.data))
-                localStorage.setItem("data", JSON.stringify(response.data))
+        console.log(" response.data" + response)
 
-            })
+        console.log(" response.data" + JSON.stringify(response.data))
+        localStorage.setItem("data", JSON.stringify(response.data))
+        this.setState({ loading: false })
+        this.setState({ next: true })
+
+
     }
     render() {
-        return (
-            <div>
-                <div className='mainpage'>
-                    <WisNavbar />
-                    <img className='bg-img' src='appstoreicon (1).png'  ></img>
+        const { loading } = this.state;
+        const { next } = this.state;
+        if (loading) {
+            return (
+                <div id="app" class="loader"></div>)
+        }
+        if (next) {
+            return (
+                <div class="wrapper">
 
-                    <div className='main-desc'>
-                        <textarea placeholder="Enter Description" className='mainpage-textarea' name='description' onChange={this.onChange}></textarea>
-                        <button className='btn btn-primary mainpage-btn' onClick={this.submitDescription}>Submit</button>
+                    <nav id="sidebar">
+                        <div class="sidebar-header">
+                            <h3>Will It Sell</h3>
+                        </div>
+
+                        <ul class="list-unstyled components">
+                            <p> Insights</p>
+
+                            <li>
+                                <a href="#userratings" data-toggle="tab" >User Ratings</a>
+
+                            </li>
+                            <li>
+                                <a href="#agegroup" data-toggle="tab" >Age Group</a>
+                            </li>
+                            <li>
+                                <a href="#predictions" data-toggle="tab" >Predictions</a>
+                            </li>
+                        </ul>
+
+                    </nav>
+
+                    <div id="content">
+
+                        <nav class="navbar navbar-default navbar-mainpage">
+                            <div class="container-fluid">
+
+                                <div class="navbar-header">
+                                    <button type="button" id="sidebarCollapse" class="btn btn-info navbar-btn">
+                                        <i class="glyphicon glyphicon-align-left"></i>
+                                        <span>Insights</span>
+                                    </button>
+                                </div>
+                            </div>
+                        </nav>
+                        <div class="tab-content">
+                            <div id="userratings" class="tab-pane fade in active">
+                                <h3>User Ratings</h3>
+                                <RatingsChart />
+                            </div>
+                            <div id="agegroup" class="tab-pane fade">
+                                <h3>Age Group </h3>
+                                <AgeGroupChart />
+                            </div>
+                            <div id="predictions" class="tab-pane fade">
+                                <h3>Predictions</h3>
+
+                            </div>
+
+                        </div>
                     </div>
-                    <div>
-                        <button className='btn btn-primary mainpage-btn' onClick={this.getRatings}>Check Ratings</button>
 
+                </div>
+
+
+            );
+        }
+        return (
+
+            <div>
+                <Navbar inverse collapseOnSelect className='navbar-fixed-top navbarlinkedin-navbar'>
+                    <Navbar.Header>
+                        <Navbar.Brand>
+                            <Link to="/home"><img alt="linkedin-logo" src="MainPage.jpeg" height="30" width="35"></img></Link>
+                        </Navbar.Brand>
+                        <Navbar.Toggle />
+                    </Navbar.Header>
+                    <Navbar.Collapse>
+
+                    </Navbar.Collapse>
+                </Navbar>
+                <div className='col-md-4 left-color'>
+                </div>
+                <div className='col-md-4'>
+
+                    <div className='container new-container'>
+                        <h1 className='main-page-h1'>Will It Sell!</h1>
+                        <textarea placeholder="Enter Description" className='mainpage-textarea' name='description' onChange={this.onChange}></textarea>
+                        <div>
+                            <button className='btn btn-primary mainpage-btn' onClick={this.submitDescription}>Submit</button>
+
+                        </div>
                     </div>
                 </div>
+                <div className='col-md-4'>
+                </div>
+
+
             </div>
-        );
+        )
     }
 }
 
